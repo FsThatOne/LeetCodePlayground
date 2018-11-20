ac31ef6e8d682f57573c67bef63964ccebcbb4b3
王正一 <wangzhengyi@meituan.com>
2018-11-20 22:31:09 +0800

    添加66.

--- LeetCode_Easy.playground/Contents.swift
+++ LeetCode_Easy.playground/Contents.swift
@@ -426,7 +426,7 @@ func lengthOfLastWord(_ s: String) -> Int {
             if hasFindWord {
                 break
             } else {
-                continue                
+                continue
             }
         } else {
             hasFindWord = true
@@ -436,3 +436,26 @@ func lengthOfLastWord(_ s: String) -> Int {
     return length
 }
 lengthOfLastWord("Hello World")
+
+//MARK: - 66. 加一
+/*给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+*/
+func plusOne(_ digits: [Int]) -> [Int] {
+    guard digits.isEmpty != true else {
+        return [1]
+    }
+    var returnArray = digits
+    if let last = returnArray.popLast() {
+        if last < 9 {
+            returnArray.append(last + 1)
+        } else {
+            var final = plusOne(returnArray)
+            final.append(0)
+            return final
+        }
+    }
+    return returnArray
+}
+plusOne([9, 9, 9, 9, 9])
