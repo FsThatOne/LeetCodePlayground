eaa918464bab11a51ed64a5e2f727c8c9029c172
WangZhengyi <wangzhengyi.211@bytedance.com>
2020-03-02 11:01:14 +0800

    flower issue

--- LeetCode_Easy.playground/Contents.swift
+++ LeetCode_Easy.playground/Contents.swift
@@ -647,6 +647,36 @@ aStack.top()
 aStack.pop()
 aStack.empty()
 
+//MARK: - 605. 种花问题 -TODO
+/*假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。*/
+func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+    guard flowerbed.count > 0 else { return n == 0 }
+    var fixedFlowers = [0] + flowerbed + [0]
+    var sum = 0
+    var index = 1
+    while index <= flowerbed.count {
+        if fixedFlowers[index - 1] == fixedFlowers[index], fixedFlowers[index] == fixedFlowers[index + 1] {
+            if fixedFlowers[index] == 0 {
+                sum += 1
+                index += 2
+            } else {
+                index += 3
+            }
+        } else if fixedFlowers[index - 1] == fixedFlowers[index + 1] {
+            if fixedFlowers[index - 1] == 0 {
+                index += 2
+            } else {
+                index += 3
+            }
+        } else {
+            index += 1
+        }
+    }
+    return sum >= n
+}
+//Time: O(n)    Space: O(n)
+canPlaceFlowers([1,0,0,0,1], 1)
+
 //MARK: - 1030. 距离顺序排列矩阵单元格
 /*
 给出 R 行 C 列的矩阵，其中的单元格的整数坐标为 (r, c)，满足 0 <= r < R 且 0 <= c < C。
